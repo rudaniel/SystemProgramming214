859a84925a998289f46ad3fc669bb3b672244dda
Daniel Lopez <ddl59@scarletmail.rutgers.edu>
2022-02-15 15:26:27 -0500

    malloc code

--- Project 1/memgrind.c
+++ Project 1/memgrind.c
@@ -4,10 +4,24 @@
 
 int main(int argc, char**argv){
 
-    int *p = malloc(1024);
+  
+
+
+
+
+
+    int *p = malloc(100);
+
+    for(int i = 0; i <100; i++){
+        p[i] = i;
+    }
+    
+    for(int i = 0; i <100; i++){
+          printf( "%d: \n", p[i]);
+    }
 
     free(p);
-    // gcc memgrind.c mymalloc.c -o filename
+    // gcc memgrind.c mymalloc.c -o test
     // ./filename
     return EXIT_SUCCESS;
 }
--- Project 1/mymalloc.c
+++ Project 1/mymalloc.c
@@ -5,11 +5,43 @@
 #define MEMSIZE 4096
 
 static char memory[MEMSIZE];
+static int MEMUSED = 0;
+
+//creating linked list
+
+//typedef struct node {
+//    int data:
+//    struct node *next;
+//}node;
+
+//struct node *linkedList; //start of the linked list
+
 
 void *mymalloc(size_t size, char *file, int line){
     printf("Mymalloc called from %s:%d\n", file, line);
+    
+    size_t trueSize = 0; //memory will be given in 8 bytes.
+    if (size % 8 == 0){
+        trueSize = size;
+    }
+    else {
+        while(size % 8 != 0){
+            size ++;
+        }
+        trueSize = size;
+    }
 
-    return NULL;
+    printf("The value of trueSize : %zu:\n", trueSize);
+    
+    if (MEMSIZE - MEMUSED < trueSize) {
+         printf("Too large for current free memory. \n");
+         return NULL;
+    }
+
+    void *memBlock = memory + MEMUSED; //A pointer to the start of free memory.
+    MEMUSED += trueSize;
+
+    return memBlock;
 } 
 
 void myfree(void *p, char *file, int line){
