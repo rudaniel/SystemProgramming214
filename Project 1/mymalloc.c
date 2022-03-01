728de02ec8d738df2293a9ee697d29f0d8e16188
yeetstatic <balitinna@gmail.com>
2022-02-28 21:15:44 -0500

    dfdf

--- Project 1/mymalloc.c
+++ Project 1/mymalloc.c
@@ -16,15 +16,11 @@ typedef struct metadata{
 
 void *mymalloc(size_t size, char *file, int line){
     printf("Mymalloc called from %s:%d\n", file, line);
-    // printf("Adding elements \n");
-    // printf("Input Size %ld\n", size);
     void *memBlock =  memory;
     metadata *data = (metadata*) memBlock;
     void *result=NULL;
     if(data->size==0 && data->free==0){
-       // printf("Fresh List\n");
         if((size+METADATA)>MEMSIZE){
-        //    printf("RETURNED NULL \n");
             return NULL;
         }
         else{
@@ -33,71 +29,43 @@ void *mymalloc(size_t size, char *file, int line){
             int temp=size+METADATA+METADATA;
             if(temp>MEMSIZE){
                 data->size=MEMSIZE-METADATA;
-          //      printf("Youre CUCKED \n");
             }
             else{
-             //   printf("size: %ld\n",size);
                 data->size=size;
                 void *freeBlock= memory+temp-METADATA;
                 metadata *nextFree= (metadata*) freeBlock;
                 nextFree->free=0;
                 nextFree->size=MEMSIZE-temp;
-            //     printf("nextsize: %ld\n",nextFree->size);
-            //     printf("datasize1: %ld\n",data->size);
              }
-           // printf("datasize2: %ld\n",data->size);
-
-            //  for(int i = 0; i<100; i++){
-            // printf("        %hhx\n", memory[i]);
-            //  }
-            // printf("    Finished Printing!\n\n");
-
             return result;
         }
     }
     else{
-       // printf("Not New List\n");
         void *lastPointer=memory+MEMSIZE-OFFSET;
         void *memBlock=memory;
         metadata *data;
         while(memBlock<=lastPointer){
             data = (metadata*) memBlock;
-           // printf("Track: %d\n",tracker);
             if(data->free == 0){
-                // printf("Inside If  Statement \n");
-                //  printf("Inside if data.size: %ld\n",data->size);
                 if(data->size>=size){
                     result=memBlock+METADATA;
-                    // printf("Inside if size: %ld\n",size);
                     int temp=size+METADATA;
-                  //  printf("Inside if temp: %d\n",temp);
                     data->free=1;
                     if(temp>data->size){
-                   //     printf("Youre CUCKED \n");
+
                     }
                     else{
-                       // printf("size: %ld\n",size);
                         void *freeBlock= memBlock+temp;
                         metadata *nextFree= (metadata*) freeBlock;
                         nextFree->free=0;
                         nextFree->size= data->size-temp;
                         data->size=size;
-                      //  printf("nextsize: %ld\n",nextFree->size);
-                      //  printf("datasize1: %ld\n",data->size);
                     }
-                  //  printf("datasize2: %ld\n",data->size);
-                  
                     break;
                 }
             }
             memBlock=memBlock+data->size+METADATA;
         }
-
-        //  for(int i = 0; i<100; i++){
-        //     printf("        %hhx\n", memory[i]);
-        // }
-        //     printf("    Finished Printing!\n\n");
-
     }
     return result;
 } 
@@ -128,9 +96,7 @@ void myfree(void *p, char *file, int line){
         return;
     }
     size_t nodeSize = data->size;
-    //printf("Free Data size Before %ld\n", data->size);
     data->free = 0;
-    //printf("Free Data size %ld\n", data->size);
     char * something = (char*) pointerAddr;
     for (int i =0; i < nodeSize; i++){
         something[i] = '\0';
@@ -144,7 +110,6 @@ void myfree(void *p, char *file, int line){
     while(memBlock<=lastPointer){
         node = (metadata*) memBlock;
         if(prev->free==0 && node->free==0){
-            //printf("merging sizes: %ld, %ld\n", prev->size, node->size);
             prev->size=prev->size+node->size+METADATA;
             memBlock=memBlock+node->size+METADATA;
             node->size=0;
@@ -154,10 +119,4 @@ void myfree(void *p, char *file, int line){
             memBlock=memBlock+node->size+METADATA;
         }
     }
-
-    // for(int i = 0; i<100; i++){
-    //     printf("        %hhx\n", memory[i]);
-    // }
-    // printf("    Finished Printing!\n\n");
-    
 }
