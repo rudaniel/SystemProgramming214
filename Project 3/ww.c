bc5528ea821bbf0f3c26183d955ce9b9e13331e0
Manav <balitinna@gmail.com>
2022-04-14 22:40:15 -0400

    jj

--- Project 3/ww.c
+++ Project 3/ww.c
@@ -511,23 +511,36 @@ void directoryExplorer(int userWidth, DIR *path, char* directory){
     }
 }
 
-void r(int width){
+void* fileW(void* w){
+    printf("Using thread- %lu\n",pthread_self());
+    int width=(int)w;
     pthread_mutex_lock(&fLock);
     if(fHead==NULL){
         pthread_mutex_unlock(&fLock);
-        return;
     }
-    int currentFile;
-    int outFile;
-    File* temp=fHead;
-    currentFile = open(temp->in,O_RDONLY);
-    outFile = open(temp->out,O_WRONLY|O_CREAT|O_TRUNC,S_IRWXU|S_IRWXG|S_IRWXO);
-    wrapper(currentFile, outFile, width);
-    close(currentFile);
-    close(outFile);
-    deleteFile();
-    pthread_mutex_unlock(&fLock);
-    r(width);
+    else{
+        int currentFile;
+        int outFile;
+        File* temp=fHead;
+        currentFile = open(temp->in,O_RDONLY);
+        outFile = open(temp->out,O_WRONLY|O_CREAT|O_TRUNC,S_IRWXU|S_IRWXG|S_IRWXO);
+        wrapper(currentFile, outFile, width);
+        close(currentFile);
+        close(outFile);
+        deleteFile();
+        pthread_mutex_unlock(&fLock);
+        fileW(w);
+    }
+    return NULL;
+}
+
+void r(int width, int threads){
+    pthread_t id;
+    for (int i = 0; i < threads; i++){
+        //printf("added thread %d...\n",i);
+        pthread_create(&id, NULL, fileW, (void*)width);
+    }
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
@@ -574,7 +587,57 @@ int main(int argc, char *argv[]){
     }
     addDirectory(argv[3]);
     makeDl();
-    r(width);
+    char* f=malloc(strlen(argv[1])+1);
+    strcpy(f,argv[1]);
+    int length=strlen(f);
+    int count=0;
+    char* token = strtok(f, ",");
+    while (token) {
+        //printf("token: %s\n", token);
+        token = strtok(NULL, ",");
+        count++;
+    }
+    free(f);
+    f=malloc(strlen(argv[1])+1);
+    strcpy(f,argv[1]);
+    //printf("Count: %d\n",count);
+    if(strcmp(f,"-r")==0){
+        printf("    Regular\n\n");
+        r(width,1);
+    }
+    else if(count==1&&f[0]=='-'&&f[1]=='r'&& length>2){
+        char num[(length-1)];
+        memcpy(num,&f[2],(length-2));
+        int threads=atoi(num);
+        printf("    Using %d Threads\n\n",threads);
+        r(width,threads);
+    }
+    else{
+        //printf("    Gotta Blast %s\n\n",f);
+        //printf("B======================================D\n");
+        char* tok = strtok(f, ",");
+        count=0;
+        char* temp;
+        int t2;
+        while (tok) {
+            //printf("token: %s\n", tok);
+            if(count==0){
+                temp= malloc(strlen(tok)+1);
+                strcpy(temp,tok);
+            }
+            else if(count==1){
+                t2=atoi(tok);
+                //printf("tt %d\n",t2);
+            }
+            count++;
+            tok = strtok(NULL, ",");
+        }
+        char num[(length-1)];
+        memcpy(num,&temp[2],(length-2));
+        int threads=atoi(num);
+        printf("    Using %d Threads for Files and %d Threads for Directories.\n\n",threads,t2);
+        r(width,threads);
+    }
     closedir(path);
     /*
     addDirectory("foo");
