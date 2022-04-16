ff98f85123e110fcc40d8e20678ae763306badc3
Manav <balitinna@gmail.com>
2022-04-15 20:12:51 -0400

    final

--- Project 3/ww.c
+++ Project 3/ww.c
@@ -28,17 +28,7 @@ Directory* dHead;
 
 pthread_mutex_t lock;
 
-
-void filePrint() {
-    File* node= fHead;
-   	while (node!=NULL) {
-        printf("    Elements in File List: %s , %s\n", node->in, node->out);
-         node= node->next;
-    }
-}
-
 void addFile(char* in, char* out){
-    //pthread_mutex_lock(&fLock);
     File* temp= (struct File*)malloc(sizeof(struct File));
     File* node= fHead;
     temp->in = malloc(strlen(in)+10);
@@ -55,11 +45,9 @@ void addFile(char* in, char* out){
         }
         node->next = temp;
     }
-    //pthread_mutex_unlock(&fLock);
 }
 
 void deleteFile(){
-    //pthread_mutex_lock(&fLock);
     if(fHead!=NULL){
         File* temp = fHead;
         fHead= fHead->next;
@@ -67,83 +55,9 @@ void deleteFile(){
         free(temp->out);
         free(temp);
     }
-    //pthread_mutex_unlock(&fLock);
-}
-
-/*void makeFl(){
-    Directory* node = dHead;
-    while (node != NULL) {
-        char* directory=malloc(strlen(node->in)+10);
-        strcpy(directory, node->in);
-        //printf("Current Dirct: %s\n", directory);
-        struct dirent *dir;
-        DIR *path=opendir(directory);
-        while((dir=readdir(path))!=NULL){
-            int len = strlen(dir->d_name);
-            char *d=(char*)malloc(len*sizeof(char)+10);
-            strcpy(d,dir->d_name);
-            //printf("Name of Path: %s\n", d);
-            if( d[0] == '.'){
-            }
-            else if(len > 5 &&
-            d[0] == 'w' &&
-            d[1] == 'r' &&
-            d[2] == 'a' &&
-            d[3] == 'p' &&
-            d[4] == '.' ){
-            }
-            else if (len > 4                     &&
-            d[len - 4] == '.' &&
-            d[len - 3] == 't' &&
-            d[len - 2] == 'x' &&
-            d[len - 1] == 't' ){
-                char* inN=(char*)malloc(len+strlen(directory)+10);
-                int l=sizeof(inN); 
-                memset(inN, 0, l);
-                strcat(inN, directory);
-                if(directory[strlen(directory) - 1] != '/'){
-                    strcat(inN, "/");
-                }
-                strcat(inN, d);
-                char* out=(char*)malloc(len+strlen(directory)+10);
-                l=sizeof(inN);
-                memset(out, 0, l);
-                strcat(out, directory);
-                if(directory[strlen(directory) - 1] != '/'){
-                    strcat(out, "/");
-                }
-                strcat(out,"wrap.");
-                strcat(out, d);
-                //printf("Name of File Path: %s\n", inN);
-                //printf("Name of File Path: %s\n", out);
-                addFile(inN, out);
-                free(out);
-                free(inN);
-                
-            }
-            free(d);
-        }
-        //printf("%s %s\n", node->in,node->out);
-        node = node->next;
-        closedir(path);
-        free(directory);
-    }
-}
-*/
-/*Directory queue*/
-
-void dirPrint() {
-    Directory* node= dHead;
-   	while (node!=NULL) {
-        printf("    Elements in Directory List: %s\n", node->in); //Printing list is not working? My be adding elements wrong?
-        //printf("Start: %d\n", start);
-        //start++;
-        node = node->next;
-    }
 }
 
 void addDirectory(char* in){
-    //pthread_mutex_lock(&dLock);
     Directory* temp= (struct Directory*)malloc(sizeof(struct Directory));
     Directory* node= dHead;
     temp->in = (char*)malloc(strlen(in)+10);
@@ -158,26 +72,21 @@ void addDirectory(char* in){
         }
         node->next = temp;
     }
-    //pthread_mutex_unlock(&dLock);
 }
 
 void deleteDirectory(){
-    //pthread_mutex_lock(&dLock);
     if(dHead!=NULL){
         Directory* temp = dHead;
         dHead= dHead->next;
         free(temp->in);
         free(temp);
     }
-    //pthread_mutex_unlock(&dLock);
 }
 int thread=0;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void* makeDl(){
-    //int infiniteLoop = 0;
     pthread_mutex_lock(&dLock);
-    //printf("Threads: %d\n",threads);
     if(dHead==NULL){
         thread--;
         while (dHead==NULL && thread > 0) {
@@ -223,7 +132,6 @@ void* makeDl(){
                 addDirectory(inN);
                 pthread_cond_broadcast(&cond);
                 pthread_mutex_unlock(&dLock);
-                //printf("added: %s\n",node->in);
             }
             else if(len > 5 && d[0] == 'w' && d[1] == 'r' && d[2] == 'a' && d[3] == 'p' && d[4] == '.' ){
             }
@@ -245,8 +153,6 @@ void* makeDl(){
                 }
                 strcat(out,"wrap.");
                 strcat(out, d);
-                //printf("Name of File Path: %s\n", inN);
-                //printf("Name of File Path: %s\n", out);
                 pthread_mutex_lock(&fLock);
                 addFile(inN, out);
                 pthread_mutex_unlock(&fLock);
@@ -262,63 +168,6 @@ void* makeDl(){
     makeDl();
     return NULL;
 }
-
-/*typedef struct dQueue {
-    directory *front;
-    directory *end;
-    int size;
-    pthread_mutex_t lock;
-    pthread_cond_t enqueue_ready, dequeue_ready;
-} dQueue;
-
-int queue_init(dQueue *q)
-{
-    q->front = NULL;
-    q->end = NULL;
-    q->size = 0;
-    pthread_mutex_init(&q->lock, NULL);
-    pthread_cond_init(&q->enqueue_ready, NULL);
-    pthread_cond_init(&q->dequeue_ready, NULL);
-    return 0;
-}
-
-int enqueue(int n, dQueue *q)
-{
-    pthread_mutex_lock(&q->lock);
-    while (q->full) {
-        pthread_cond_wait(&q->enqueue_ready, &q->lock);
-    }
-    q->data[q->stop] = n;
-    q->stop++;
-    if (q->stop == QUEUESIZE) q->stop = 0;
-    if (q->start == q->stop) q->full = 1;
-    pthread_cond_signal(&q->dequeue_ready);
-    pthread_mutex_unlock(&q->lock);
-    return 0;
-}
-
-int dequeue(int *n, dQueue *q)
-{
-    pthread_mutex_lock(&q->lock);
-    while (!q->full && q->start == q->stop) {
-        pthread_cond_wait(&q->dequeue_ready, &q->lock);
-    }
-    * n = q->data[q->start];
-    q->start++;
-    if (q->start == QUEUESIZE) q->start == 0;
-    q->full = 0;
-    pthread_signal(&q->enqueue_ready);
-    pthread_mutex_unlock(&q->lock);
-    return 0;
-}
-*/
-/*
-*
-*
-*END OF DIRECTORY QUEUE CODE
-*
-*
-*/
 
 void wrapper(int in, int out,int userWidth){
     if(in==-1&&out==-1)
@@ -562,69 +411,28 @@ void* fileW(void* w){
     free(in);
     free(out);
     fileW(w);
-
     return NULL;
 }
 
 void r(int width, int f, int d){
     pthread_t dIds[d];
     pthread_t fIds[f];
-    //pthread_t id;
     for (int i = 0; i < d; i++){
-        //printf("added thread %d...\n",i);
         thread++;
         pthread_create(&dIds[i], NULL, makeDl, NULL);
     }
-    for (int i = 0; i < d; i++){ 
-        //printf("added thread %d...\n",i);
+    for (int i = 0; i < d; i++){
         pthread_join(dIds[i], NULL);
     }
     for (int i = 0; i < f; i++){
-        //printf("added thread %d...\n",i);
         pthread_create(&fIds[i], NULL, fileW, &width);
     }
     for (int i = 0; i < f; i++){
-        //printf("added thread %d...\n",i);
         pthread_join(fIds[i], NULL);
     }
-    /*for (int i = 0; i < (f+d); i++){
-       //pthread_join(ids[i], NULL);
-    }*/
-   // pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
-   /* if(argc<1){
-        printf("Invalid number of argumments\n");
-        return-1;
-    }
-    else if(argc==2){
-        printf("Enter text now:(Press ctrl d to run word wrapper)\n");
-        wrapper(0, -1, atoi(argv[1]));
-    }
-    else {
-        int userWidth =  atoi(argv[1]);
-        DIR *path;
-        path = opendir(argv[2]);
-        int rd = open(argv[2],O_RDONLY);
-        struct stat dirFile;
-        int status = stat (argv[2], &dirFile);
-        if(status != 0){
-            close(rd);
-            closedir(path);
-            perror("The Second Console Argument is INVALID Due to: \n  --Invaild Directory \n          OR \n  --File Doesn't Exist \n");
-            exit(EXIT_FAILURE);
-        }
-        else if(S_ISREG(dirFile.st_mode)){
-            wrapper(rd, -1,userWidth);
-        }
-        else if(S_ISDIR(dirFile.st_mode)){
-            directoryExplorer(userWidth, path, argv[2]);
-        }
-        close(rd);
-        closedir(path);
-    }*/
-
     int width =  atoi(argv[2]);
     DIR *path;
     path = opendir(argv[3]);
@@ -636,51 +444,43 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
     addDirectory(argv[3]);
-    //makeDl();
     char* f=malloc(strlen(argv[1])+1);
     strcpy(f,argv[1]);
     int length=strlen(f);
     int count=0;
     char* token = strtok(f, ",");
     while (token) {
-        //printf("token: %s\n", token);
         token = strtok(NULL, ",");
         count++;
     }
     free(f);
     f=malloc(strlen(argv[1])+1);
     strcpy(f,argv[1]);
-    //printf("Count: %d\n",count);
     if(strcmp(f,"-r")==0){
         printf("    Regular\n\n");
         r(width,1,1);
     }
     else if(count==1&&f[0]=='-'&&f[1]=='r'&& length>2){
         char* num= malloc(length+1);
-        //memset(num,0,strlen(num));
         memcpy(num,&f[2],(length-2));
         int threads=atoi(num);
         free(num);
         free(f);
         printf("    Using %d Threads\n\n",threads);
-        r(width,1,threads);
+        r(width,threads,1);
     }
     else{
-        //printf("    Gotta Blast %s\n\n",f);
-        //printf("B======================================D\n");
         char* tok = strtok(f, ",");
         count=0;
         char* temp;
         int t2;
         while (tok) {
-            //printf("token: %s\n", tok);
             if(count==0){
                 temp= malloc(strlen(tok)+1);
                 strcpy(temp,tok);
             }
             else if(count==1){
                 t2=atoi(tok);
-                //printf("tt %d\n",t2);
             }
             count++;
             tok = strtok(NULL, ",");
@@ -694,46 +494,10 @@ int main(int argc, char *argv[]){
         r(width,t2,threads);
     }
     closedir(path);
-    /*
-    addDirectory("foo");
-    makeDl();
-   // makeFl();
-    dirPrint();
-    filePrint();
-*/
-    //wipe directory
-    //printf("dddddddd\n");
-    //dirPrint();
     while (dHead != NULL) {
         deleteDirectory();
     }
     while (fHead != NULL) {
         deleteFile();
     }
-    
-//pthread_exit(NULL);
-   /* makeFl();
-    File* node = fHead;
-    while (node != NULL) {
-        printf("%s %s\n", node->in,node->out);
-        node = node->next;
-    }
-    printf("\n\n");
-    deleteFile();
-    node = fHead;
-    while (node != NULL) {
-        printf("%s %s\n", node->in,node->out);
-        node = node->next;
-    }
-    node = fHead;
-    for(int i=10; i>0;i--) {
-        deleteFile();
-    }
-printf("new lsit\n");
-    node = fHead;
-    while (node != NULL) {
-        printf("%s %s\n", node->in,node->out);
-        node = node->next;
-    }
-    printf("\n\n");*/
 }
