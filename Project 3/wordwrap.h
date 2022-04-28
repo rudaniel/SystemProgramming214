a21a58ff446dfdfb9afe2eb7c2c05b6d75ab76e8
Manav <balitinna@gmail.com>
2022-04-28 01:45:23 -0400

    me go sleep

---
+++ Project 3/wordwrap.h
@@ -0,0 +1,25 @@
+typedef struct File{
+    char* in;
+    char* out;    
+    struct File *next;
+} File;
+
+typedef struct Directory{
+    char* in; 
+    struct Directory *next;
+} Directory;
+
+extern File* fHead;
+extern Directory* dHead;
+
+extern int fCount;
+extern int dCount;
+
+void addFile(char* in, char* out);
+void deleteFile();
+void addDirectory(char* in);
+void deleteDirectory();
+void* makeDl(void* r);
+void wrapper(int in, int out,int userWidth);
+void* fileW(void* w);
+void r(int width, int f, int d);
